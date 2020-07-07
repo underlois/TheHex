1f580f8d776e99e5c046bf09a687d973d09297b3
Curtis <Curtishollidge@gmail.com>
2020-07-07 15:21:40 +0100

    TheHexBeginning
    The Start

---
+++ Assets/GameMechanic/OrbRespawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbRespawn : MonoBehaviour {
+
+
+    private void Update()
+    {
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+		Destroy(this);
+    }
+
+
+}
